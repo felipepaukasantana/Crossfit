f2938bd0da26679b90eb298517152b15f46e7fe6
fpsantana <felipepauka@gmail.com>
2020-10-05 17:20:21 -0300

    Updates

---
+++ precache-manifest.43930ee1d314830561638ae3601e8439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d17c12f4da9e45f98ad3a7b625f724a",
+    "url": "/Crossfit/index.html"
+  },
+  {
+    "revision": "16a3b63a521e16a5a809",
+    "url": "/Crossfit/static/css/main.3e882411.chunk.css"
+  },
+  {
+    "revision": "41580020bfc1d7a882e6",
+    "url": "/Crossfit/static/js/2.56c86558.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Crossfit/static/js/2.56c86558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a3b63a521e16a5a809",
+    "url": "/Crossfit/static/js/main.ba3362b8.chunk.js"
+  },
+  {
+    "revision": "ee200e4974b624f64c3b",
+    "url": "/Crossfit/static/js/runtime-main.8c0b6a1a.js"
+  }
+]);
